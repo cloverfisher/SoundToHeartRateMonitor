23fa0bf0430ded9a4b87ad97bc45e4e4e0018c40
cloverfisher <yuysyu@gmail.com>
2014-12-10 17:33:25 -0500

    now can shall by mail

--- src/shiyaoyu/me/soundtoheartratemonitor/MainActivity.java
+++ src/shiyaoyu/me/soundtoheartratemonitor/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends Activity {
 			}
 		});
 		mainToListViewButton.setEnabled(false);
+		mainToListViewButton.setVisibility(4);
 
 	}
 
--- src/shiyaoyu/me/soundtoheartratemonitor/RecordDisplayActivity.java
+++ src/shiyaoyu/me/soundtoheartratemonitor/RecordDisplayActivity.java
@@ -1,22 +1,21 @@
 package shiyaoyu.me.soundtoheartratemonitor;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
@@ -24,15 +23,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.media.AudioFormat;
-import android.media.AudioManager;
 import android.media.AudioRecord;
-import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -80,14 +78,13 @@ public class RecordDisplayActivity extends Activity{
 	int beats = 0;
 	int threshold = 1000;
 	boolean thresholdflag = false;
-    File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/FHR.pcm");
+    File file;// = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/FHR.pcm");
     
-//    private XYPlot fhrPlot = null; 
-//    private SimpleXYSeries fhrSeries = null;
     Redrawer redrawer;
     LinearLayout mylayout;
     LinearLayout scrollLayout;
 
+    Button shareButton ;
 	
 	TextView beatsTextView = null;
 	long endTime = System.currentTimeMillis();
@@ -101,6 +98,8 @@ public class RecordDisplayActivity extends Activity{
 	private SimpleXYSeries myseries[];
 	private ScrollView myScrollView[];
 	
+	
+	String filenameString;
 	class RecordButton extends Button{
 
 		
@@ -131,8 +130,41 @@ public class RecordDisplayActivity extends Activity{
 	{
 		Intent intent = new Intent();
 		intent.setAction(Intent.ACTION_VIEW);
+		
 		intent.setDataAndType(Uri.parse("file://" + uri), "image/*");
 		startActivity(intent);
+	}
+	
+	public void sharePicture(String uriString)
+	{
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("image/*");
+		Uri uri = Uri.parse("file://" + uriString);
+		List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 0);
+		if(!list.isEmpty())
+		{
+			 List<Intent> targetedShareIntents = new ArrayList<Intent>();
+			Intent targetIntent = new Intent(Intent.ACTION_SEND);
+			targetIntent.setType("image/*");
+			for(ResolveInfo info :list)
+			{
+				ActivityInfo activityInfo = info.activityInfo;
+				if(activityInfo.name.contains("mail")||activityInfo.packageName.contains("mail"))
+				{
+					targetIntent.putExtra(Intent.EXTRA_STREAM, uri);
+					targetIntent.setPackage(activityInfo.packageName);
+					targetedShareIntents.add(targetIntent);
+				}
+			}
+			Intent chooserIntent = Intent.createChooser(targetedShareIntents.remove(0), "Select app to share");
+			chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray(new Parcelable[] {}));
+
+			startActivity(chooserIntent);
+
+		}
+//		Log.e("ysy", filenameString);
+//		intent.putExtra(android.content.Intent.EXTRA_TEXT, "test");  
+//		intent.putExtra(Intent.EXTRA_STREAM, uri);
 	}
 	
 	private void creatAudioRecord()
@@ -174,6 +206,7 @@ public class RecordDisplayActivity extends Activity{
 				double time=0;
 				double timerecord = 0;
 				int bpm = 0;
+				int j= 0;
 				while(isRecord == true)
 				{
 					readsize = audioRecord.read(audiodata, 0, bufferSizeInByte);
@@ -187,8 +220,16 @@ public class RecordDisplayActivity extends Activity{
 							time = endTime - startTime;
 							if(time > timerecord + 250)
 							{
+								j++;
 					        	Log.e(LOG_TAG, "time:" + time + "timerecord:" + timerecord);
-								bpm = (int) (60*1000/(time-timerecord));
+								
+								if(j>2)
+								{
+									bpm=(bpm+(int) (60*1000/(time-timerecord)))/2;
+								}
+								else {
+									bpm = (int) (60*1000/(time-timerecord));
+								}
 								timerecord = time;
 								beats++;							
 								Message msgMessage = new Message();
@@ -199,8 +240,11 @@ public class RecordDisplayActivity extends Activity{
 					        	Log.e(LOG_TAG, "time:" + time/1000);
 					            timeList.add(time/1000);
 					            bpmList.add(bpm);
-					      //      fhrSeries.addLast(time/1000, bpm);	
-					            if(time/1000<=HISTORY_SIZE)
+					            if(bpm<30||bpm>240)
+					            {
+					            	continue;
+					            }
+					            else if(time/1000<=HISTORY_SIZE)
 					            {
 					            	myseries[0].addLast(time/1000, bpm);
 					            }
@@ -210,11 +254,7 @@ public class RecordDisplayActivity extends Activity{
 					            else {
 					            	myseries[2].addLast(time/1000, bpm);
 								}
-
-							}
-
-							
-							
+							}							
 						}
 						else if (audiodata[i]<threshold) {
 							thresholdflag = false;
@@ -228,9 +268,9 @@ public class RecordDisplayActivity extends Activity{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		
+		}		
 	}
+	
 	private void startRecording(){
 		creatAudioRecord();
 		isRecord = true;
@@ -261,197 +301,27 @@ public class RecordDisplayActivity extends Activity{
 	
     /*
      * Handler ,send the message from record thread to the main thread
-     * */
-    
+     * */  
     private Handler beatsHandler = new Handler(){
 
 		@Override
-		public void handleMessage(Message msg) {
-		
+		public void handleMessage(Message msg) {		
 			super.handleMessage(msg);
 			switch (msg.what) {
 			case 1:
-			{
-				
+			{				
 				beatsTextView.setText("" + beats);
 				break;
 			}
 			default:
 				break;
 			}
-		}
-    	
+		}  	
     };
     
-    /*
-     * replay the record
-     * */
-    
-    public void play() {
-        // Get the file we want to playback.
-        // Get the length of the audio stored in the file (16 bit so 2 bytes per short)
-        // and create a short array to store the recorded audio.
-    	Log.e(LOG_TAG, "play start");
-        int musicLength = (int)(file.length()/2);
-        short[] music = new short[musicLength];
-   
-   
-        try {
-          // Create a DataInputStream to read the audio data back from the saved file.
-          InputStream is = new FileInputStream(file);
-          BufferedInputStream bis = new BufferedInputStream(is);
-          DataInputStream dis = new DataInputStream(bis);  
-           
-          // Read the file into the music array.
-          int i = 0;
-          while (dis.available() > 0) {
-            music[i] = dis.readShort();
-            i++;
-          }
-   
-   
-          // Close the input streams.
-          dis.close();    
-   
-   
-          // Create a new AudioTrack object using the same parameters as the AudioRecord
-          // object used to create the file.
-          AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-        		  									SAMPLE_RATE_IN_HZ,
-                                                 AudioFormat.CHANNEL_IN_MONO,
-                                                 AudioFormat.ENCODING_PCM_16BIT,
-                                                 musicLength*2,
-                                                 AudioTrack.MODE_STREAM);
-          // Start playback
-          audioTrack.play();
-       
-          // Write the music buffer to the AudioTrack object
-          audioTrack.write(music, 0, musicLength);
-   
-          audioTrack.stop() ;
-   
-        } catch (Throwable t) {
-          Log.e("AudioTrack","Playback Failed");
-        }
-    	Log.e(LOG_TAG, "play end");
-      }
+  
 
-	
-    /*
-     * oncreate 
-     * */
-    
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	 	width = getWindowManager().getDefaultDisplay().getWidth();
-	 	height = getWindowManager().getDefaultDisplay().getHeight();
-	 	offset = 0;
-	 	myxyplot = new XYPlot[3];
-	 	myseries = new SimpleXYSeries[3];
-	 	myScrollView = new ScrollView[3]; 
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); 
-		
-		setContentView(R.layout.activity_record);	
-		sv = (ScrollView)findViewById(R.id.scrollView1);
-		mylayout = (LinearLayout)findViewById(R.id.plotLinearlayout);
-		scrollLayout = (LinearLayout)findViewById(R.id.scrolllayout1);
-
-
-		
-		beatsTextView = (TextView)findViewById(R.id.beattext);
-		beatsTextView.setText(""+ beats);
-		recordButton = (Button)findViewById(R.id.btnRecord);
-		recordButton.setText("Start");
-		recordButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Log.e(LOG_TAG, "onclick");
-				if(recordFlag)
-				{
-					exportButton.setEnabled(false);
-					recordButton.setText("Stop");
-				}				
-				else {
-					exportButton.setEnabled(true);
-					recordButton.setText("Start");
-				}
-				onRecord(recordFlag);
-				recordFlag = !recordFlag;			
-			}
-		});
-
-		
-		exportButton = (Button)findViewById(R.id.btnexport);
-		exportButton.setEnabled(false);
-		exportButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-			//	play();
-				if(timeList.size()>4)
-				{					
-					Bitmap bmpplot[]= new Bitmap[3];
-					for(int i = 0; i < 3; i++)
-					{
-						Log.e(LOG_TAG, "asd"+i);
-						myxyplot[i].setDrawingCacheEnabled(true);
-				        int width = myxyplot[i].getWidth();
-				        int height = myxyplot[i].getHeight();
-				        myxyplot[i].measure(width, height);
-				        bmpplot[i] = Bitmap.createBitmap(myxyplot[i].getDrawingCache());
-				        myxyplot[i].setDrawingCacheEnabled(false);
-	
-					}
-			        int width = myxyplot[0].getWidth();
-			        int height =myxyplot[0].getHeight()*3;
-
-			        Bitmap btm = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
-			        Canvas canvas = new Canvas(btm);
-			        canvas.drawBitmap(bmpplot[0], 0,0, null);
-			        canvas.drawBitmap(bmpplot[1], 0, bmpplot[0].getHeight(), null);
-			        canvas.drawBitmap(bmpplot[2], 0, bmpplot[0].getHeight()*2, null);
-			        canvas.drawBitmap(btm, 0, 0, null);
-			        FileOutputStream fos;
-			    	File file2 = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/fhrPhoto.png");
-			    	if(file2.exists())
-			    		file2.delete();
-					try {
-					//	File pbgfile = new File("fhrPhoto.png"); 
-						fos = new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/fhrPhoto.png", true);
-						btm.compress(CompressFormat.PNG, 100, fos);
-						openPicture(Environment.getExternalStorageDirectory().getAbsolutePath() + "/fhrPhoto.png");
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-
-			}
-		});
-		
-		/*plot part*/
-	//	fhrPlot = (XYPlot)findViewById(R.id.xyplot1);
-		scroll_width = scrollLayout.getWidth();
-		rate = scroll_width/HISTORY_SIZE;
-		Log.e(LOG_TAG, "plotwidth:"+scroll_width+" rate "+ rate);
-	//	PlotConfigure.plotConfiguration(fhrPlot, 0,0+HISTORY_SIZE);
-		
-		myxyplot[0] = (XYPlot)findViewById(R.id.xyplot1);
-		myxyplot[1] = (XYPlot)findViewById(R.id.xyplot2);
-		myxyplot[2] = (XYPlot)findViewById(R.id.xyplot3);
-
-		PlotConfigure.plotConfiguration(myxyplot[0], 0,0+HISTORY_SIZE);
-		PlotConfigure.plotConfiguration(myxyplot[1], HISTORY_SIZE,2*HISTORY_SIZE);	
-		PlotConfigure.plotConfiguration(myxyplot[2], 2*HISTORY_SIZE,3*HISTORY_SIZE);		
-//		fhrPlot.scrollBy(-width+100, 0);
-	    addNewPlot();		
-	}
-	
-
-
+	//This makes the view not recreate when the phone rotate.
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		// TODO Auto-generated method stub
@@ -484,6 +354,137 @@ public class RecordDisplayActivity extends Activity{
                 Arrays.asList(new Plot[]{myxyplot[0],myxyplot[1],myxyplot[2]}),
                 100, false);
 	}
+    
+    
+    /*
+     * oncreate 
+     * */
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	 	width = getWindowManager().getDefaultDisplay().getWidth();
+	 	height = getWindowManager().getDefaultDisplay().getHeight();
+	 	offset = 0;
+	 	myxyplot = new XYPlot[3];
+	 	myseries = new SimpleXYSeries[3];
+	 	myScrollView = new ScrollView[3]; 
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); 
+		setContentView(R.layout.activity_record);	
+		sv = (ScrollView)findViewById(R.id.scrollView1);
+		mylayout = (LinearLayout)findViewById(R.id.plotLinearlayout);
+		scrollLayout = (LinearLayout)findViewById(R.id.scrolllayout1);
+
+		
+		beatsTextView = (TextView)findViewById(R.id.beattext);
+		beatsTextView.setText(""+ beats);
+		recordButton = (Button)findViewById(R.id.btnRecord);
+		recordButton.setText("Start");
+		recordButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Log.e(LOG_TAG, "onclick");
+				if(recordFlag)
+				{
+					filenameString = Environment.getExternalStorageDirectory().getAbsolutePath() + "/fhrPhoto.png";
+					file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/FHR.wav");
+					exportButton.setEnabled(false);
+					recordButton.setText("Stop");
+				}				
+				else {
+					exportButton.setEnabled(true);
+					recordButton.setText("Start");
+				}
+				onRecord(recordFlag);
+				recordFlag = !recordFlag;			
+			}
+		});
+
+		shareButton = (Button)findViewById(R.id.btnshare);
+		shareButton.setEnabled(false);
+		shareButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				sharePicture(filenameString);
+				
+				
+			}
+		});
+		
+		exportButton = (Button)findViewById(R.id.btnexport);
+		exportButton.setEnabled(false);
+		exportButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			//	play();
+				if(timeList.size()>4)
+				{					
+					Bitmap bmpplot[]= new Bitmap[3];
+					for(int i = 0; i < 3; i++)
+					{
+						Log.e(LOG_TAG, "asd"+i);
+						myxyplot[i].setDrawingCacheEnabled(true);
+				        int width = myxyplot[i].getWidth();
+				        int height = myxyplot[i].getHeight();
+				        myxyplot[i].measure(width, height);
+				        bmpplot[i] = Bitmap.createBitmap(myxyplot[i].getDrawingCache());
+				        myxyplot[i].setDrawingCacheEnabled(false);
+	
+					}
+			        int width = myxyplot[0].getWidth();
+			        int height =myxyplot[0].getHeight()*3;
+
+			        Bitmap btm = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
+			        Canvas canvas = new Canvas(btm);
+			        canvas.drawBitmap(bmpplot[0], 0,0, null);
+			        canvas.drawBitmap(bmpplot[1], 0, bmpplot[0].getHeight(), null);
+			        canvas.drawBitmap(bmpplot[2], 0, bmpplot[0].getHeight()*2, null);
+			        canvas.drawBitmap(btm, 0, 0, null);
+			        FileOutputStream fos;
+			    	File file2 = new File(filenameString);
+			    	if(file2.exists())
+			    		file2.delete();
+					try {
+					//	File pbgfile = new File("fhrPhoto.png"); 
+						fos = new FileOutputStream(filenameString, true);
+						btm.compress(CompressFormat.PNG, 100, fos);
+						openPicture(filenameString);
+						shareButton.setEnabled(true);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+
+			}
+		});
+		
+		/*plot part*/
+	//	fhrPlot = (XYPlot)findViewById(R.id.xyplot1);
+		scroll_width = scrollLayout.getWidth();
+		rate = scroll_width/HISTORY_SIZE;
+		Log.e(LOG_TAG, "plotwidth:"+scroll_width+" rate "+ rate);
+	//	PlotConfigure.plotConfiguration(fhrPlot, 0,0+HISTORY_SIZE);
+		
+		myxyplot[0] = (XYPlot)findViewById(R.id.xyplot1);
+		myxyplot[1] = (XYPlot)findViewById(R.id.xyplot2);
+		myxyplot[2] = (XYPlot)findViewById(R.id.xyplot3);
+
+		PlotConfigure.plotConfiguration(myxyplot[0], 0,0+HISTORY_SIZE);
+		PlotConfigure.plotConfiguration(myxyplot[1], HISTORY_SIZE,2*HISTORY_SIZE);	
+		PlotConfigure.plotConfiguration(myxyplot[2], 2*HISTORY_SIZE,3*HISTORY_SIZE);		
+//		fhrPlot.scrollBy(-width+100, 0);
+	    addNewPlot();		
+	}
+	
+
+
+
 
 	
 }
